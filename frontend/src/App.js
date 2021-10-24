186f7e21c3e950ec411b1022f9133393744e6d69
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2021-10-24 02:50:14 -0700

    Fix scrolling and add url input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,74 +9,50 @@ import {
   Grid,
   GridItem,
   Heading,
+  Input,
   Select,
   FormControl,
   AspectRatio,
   theme,
+  useToast,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
+import { validURL } from './utils';
 
 function App() {
-  const [currentDate, setCurrentDate] = useState();
+  const [currentURL, setCurrentURL] = useState();
+  const toast = useToast();
+
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <Grid h="100vh" templateColumns="repeat(3, 1fr)">
         <GridItem colSpan={2} bg="white">
-          <AspectRatio ratio={1}>
-            <iframe title="snapshot" src="https://example.com" />
-          </AspectRatio>
+          {validURL(currentURL) ? (
+            <AspectRatio ratio={5} h="100vh">
+              <iframe title="snapshot" src={currentURL} />
+            </AspectRatio>
+          ) : null}
         </GridItem>
         <GridItem boxShadow="2xl" colSpan={1} p={4} bg="white">
           <Heading
-            //     bgGradient="linear(to-l, #7928CA, #FF0080)"
-            //     bgClip="text"
-            fontSize="5xl"
+            align="center"
+            fontSize="xl"
             fontWeight="extrabold"
             marginBottom={5}
           >
-            Inter-timline
-            <br />
-            Commenting
-            <br />
-            System
+            Inter-Timeline Commenting System
           </Heading>
-          <FormControl>
-            <Select
-              placeholder="Latest"
-              value={currentDate}
-              size="lg"
-              onChange={event => setCurrentDate(event.currentTarget.value)}
-            >
-              <option value="option1">Option 1</option>
-              <option value="option2">Option 2</option>
-              <option value="option3">Option 3</option>
-            </Select>
-          </FormControl>
-          <Text>{currentDate}</Text>
+          <Input
+            variant="outline"
+            placeholder="Website URL"
+            onKeyDown={e => {
+              if (e.key === 'Enter') setCurrentURL(e.currentTarget.value);
+            }}
+            isInvalid={currentURL !== null && !validURL(currentURL)}
+          />
         </GridItem>
-        {/* <GridItem rowSpan={2} colSpan={1} bg="blue" /> */}
       </Grid>
-      {/* <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box> */}
     </ChakraProvider>
   );
 }
---
+++ frontend/src/utils/index.js
@@ -0,0 +1,5 @@
+export function validURL(s) {
+  var regexp =
+    /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+  return regexp.test(s);
+}
