88dd883f2cdf6e3d12f1e482da78a82fd88a7151
Brian Phan <b.phan@berkeley.edu>
2021-10-24 01:49:25 -0700

    Connect db to archiver

--- db/app.js
+++ db/app.js
@@ -1,3 +1,4 @@
+const fetch = require("node-fetch");
 const express = require("express");
 const IpfsClient = require("ipfs-http-client");
 const OrbitDB = require("orbit-db");
@@ -5,45 +6,34 @@ const app = express();
 const port = 3000;
 
 const ipfs = IpfsClient.create();
-// const projectId = "1zwEmKfT6i3cKKdlFee4rTQyHB8";
-// const projectSecret = "a066b3b0b6fefb67c3c8937f63a045c5";
-// const auth = projectId + ":" + projectSecret;
-
-// const ipfs = IpfsClient.create({
-//   host: "ipfs.infura.io",
-//   port: 5001,
-//   protocol: "https",
-//   headers: {
-//     authorization: auth,
-//   },
-// });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/newSnapshot/:url?", async (req, res) => {
-  // Caught and passed down to the errorHandler middleware
   const { url } = req.params;
 
-  const timestamp = Date.now();
-  const snapshotHash = null;
+  const data = await fetch(
+    `https://localhost:5000/archive?url=${encodeURI(url)}`
+  );
+  const { timestamp, location } = await data.json();
 
   const orbitdb = await OrbitDB.createInstance(ipfs);
 
   const db = await orbitdb.keyvalue("snapshot-details-" + url.toString());
   await db.load();
 
-  const hash = await db.put(timestamp, { snapshotHash });
+  const hash = await db.put(timestamp, { location });
   console.log(hash);
 
-  //   const value = db.get(timestamp);
-  //   console.log(value);
+  const value = db.get(timestamp);
+  console.log(value);
 
   //   const allValues = db.all;
   //   console.log(allValues);
 
-  res.send({ timestamp, snapshotHash });
+  res.send({ timestamp, location });
   await orbitdb.disconnect();
 });
 
